3b462ffa6bee7e6388d78a9eaf782cb89f716045
bitflower <matthias.max@bitflower.net>
2020-07-27 09:39:16 +0200

    finetune tests

--- src/index.ts
+++ src/index.ts
@@ -12,35 +12,36 @@ import { rules } from './rules';
 const ability = buildAbility(rules);
 
 // Do the checks
-const canEditDisallowedService = ability.can('edit', 'alarme');
-// 'The ability allows editing of other subjects'
 
-const canEditAllowedService = ability.can('edit', 'wartungen');
-// 'The ability does not allow editing of subject "wartungen"'
+// The ability does not allows editing of other subjects then "wartungen"
+const canEditDisallowedServiceExpectFalse = ability.can('edit', 'alarme');
 
-const canEditDisallowedRecord = ability.cannot(
+// The ability allows editing of subject "wartungen"
+const canEditAllowedServiceExpectTrue = ability.can('edit', 'wartungen');
+
+// The ability does not allow editing of "wartungen" that don"t match the conditions
+const cannotEditDisallowedRecordExpectFalse = ability.cannot(
   'edit',
   subject('wartungen', disallowedDummyWartung)
 );
-// `The ability allows editing of "wartungen" that don"t match the conditions (_id: ${disallowedDummyWartung._id})`
 
-const canEditAllowedRecord = ability.can(
+// The ability allows editing of "wartungen" that match the conditions (_id: ${dummyWartung._id})`
+const canEditAllowedRecordExpectTrue = ability.can(
   'edit',
   subject('wartungen', dummyWartung)
 );
-// `The ability does not allow editing of "wartungen" that match the conditions (_id: ${dummyWartung._id})`
 
-const canEditDisallowedRecordWithField = ability.cannot(
+// The ability does not allow editing of "wartungen" that don't match the conditions (name $eq 'Wartung mit Sound') on field "notificationSoundId"`
+const cannotEditDisallowedRecordWithFieldExpectFalse = ability.cannot(
   'edit',
   subject('wartungen', disallowedDummyWartungWithFIeld),
   'notificationSoundId'
 );
-// `The ability does allow editing of "wartungen" that don't match the conditions (name: ${disallowedDummyWartungWithFIeld.name}) on field "notificationSoundId"`
 
 console.log('RESULT', {
-  canEditDisallowedService,
-  canEditAllowedService,
-  canEditDisallowedRecord,
-  canEditAllowedRecord,
-  canEditDisallowedRecordWithField
+  canEditDisallowedServiceExpectFalse,
+  canEditAllowedServiceExpectTrue,
+  cannotEditDisallowedRecordExpectFalse,
+  canEditAllowedRecordExpectTrue,
+  cannotEditDisallowedRecordWithFieldExpectFalse
 });
--- src/records.ts
+++ src/records.ts
@@ -1,11 +1,16 @@
+// Exmaple record that is allowed to be accessed
 export const dummyWartung = {
   _id: 'abc',
   name: 'Wartung 1'
 };
+
+// Example records with an _id that is not allowed
 export const disallowedDummyWartung = {
   _id: 'xxxx',
   name: 'Wartung 2'
 };
+
+// Example record that match "name" on a rule that disallows the access to the field "notificationSoundId"
 export const disallowedDummyWartungWithFIeld = {
   _id: 'yyyy',
   name: 'Wartung mit Sound',
